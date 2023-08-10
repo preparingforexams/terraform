f9c78466aca55ba37766ee7857e28b6fbe7c3437
Björn Petersen <git@bjoernpetersen.net>
2023-08-10 10:48:17 +0200

    Remove weird hashicorp/github provider

--- modules/repo/main.tf
+++ modules/repo/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
 provider "github" {
   owner = "preparingforexams"
 }
