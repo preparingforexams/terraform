9d911794f987bb5c3adc84d24cc869067196dd2d
Björn Petersen <git@bjoernpetersen.net>
2023-08-16 12:47:28 +0200

    Add deletespam repo

---
+++ deletespamtelegrambot.tf
@@ -0,0 +1,7 @@
+module "deletespam_repo" {
+  source              = "./modules/repo"
+  name                = "deletespamtelegrambot"
+  default_branch_name = "master"
+  enable_actions      = false
+  is_archived         = false
+}
