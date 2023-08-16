15101ffee1fca7956c3761c6d57a8ee3dec1c114
Björn Petersen <git@bjoernpetersen.net>
2023-08-16 14:46:31 +0200

    Add rate_limiter

---
+++ rate_limiter.tf
@@ -0,0 +1,12 @@
+module "rate_limiter_repo" {
+  source = "./modules/repo"
+  name   = "rate_limiter"
+  required_status_checks = [
+    "test (3.9)",
+    "test (3.10)",
+    "test (3.11)",
+    "lint (3.9)",
+    "lint (3.10)",
+    "lint (3.11)",
+  ]
+}
