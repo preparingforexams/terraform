a463f49d6d92a24c723876f2758fed9c5cdc7343
Björn Petersen <git@bjoernpetersen.net>
2023-08-22 12:28:45 +0200

    Adjust required status checks for songlinker

--- telegram-songlinker-bot.tf
+++ telegram-songlinker-bot.tf
@@ -1,4 +1,9 @@
 module "songlinker_repo" {
   source = "./modules/repo"
   name   = "telegram-songlinker-bot"
+  required_status_checks = [
+    "lint",
+    "unit-test",
+    "build-container-image",
+  ]
 }
