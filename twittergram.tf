c9dd7bc80507134ee1f2e8f5093be8e8fa87569c
Björn Petersen <git@bjoernpetersen.net>
2023-09-25 21:14:38 +0200

    Require SonarCloud to pass for Twittergram

--- twittergram.tf
+++ twittergram.tf
@@ -1,5 +1,11 @@
 module "twittergram_repo" {
-  source      = "github.com/BlindfoldedSurgery/terraform-repo-module"
-  name        = "twittergram"
-  description = "Forwards toots/mails to telegram chats"
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module"
+  name                   = "twittergram"
+  description            = "Forwards toots/mails to telegram chats"
+  required_status_checks = [
+    "lint",
+    "test",
+    "build-containter-image",
+    "SonarCloud Code Analysis",
+  ]
 }
