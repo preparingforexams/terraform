ffd6f6859b9c2b716c55ec54f3cc8219858ff440
Björn Petersen <git@bjoernpetersen.net>
2023-09-26 14:27:23 +0200

    Formatting

--- annoying-mention-echo-bot.tf
+++ annoying-mention-echo-bot.tf
@@ -1,9 +1,9 @@
 module "annoying_repo" {
-  source              = "github.com/BlindfoldedSurgery/terraform-repo-module"
-  name                = "annoying-mention-echo-bot"
-  default_branch_name = "master"
-  blocked_branches    = []
-  enable_actions      = false
-  is_archived         = true
+  source                = "github.com/BlindfoldedSurgery/terraform-repo-module"
+  name                  = "annoying-mention-echo-bot"
+  default_branch_name   = "master"
+  blocked_branches      = []
+  enable_actions        = false
+  is_archived           = true
   create_default_branch = false
 }
--- deletespamtelegrambot.tf
+++ deletespamtelegrambot.tf
@@ -1,9 +1,9 @@
 module "deletespam_repo" {
-  source              = "github.com/BlindfoldedSurgery/terraform-repo-module"
-  name                = "deletespamtelegrambot"
-  default_branch_name = "master"
-  blocked_branches    = []
-  enable_actions      = false
-  is_archived         = true
+  source                = "github.com/BlindfoldedSurgery/terraform-repo-module"
+  name                  = "deletespamtelegrambot"
+  default_branch_name   = "master"
+  blocked_branches      = []
+  enable_actions        = false
+  is_archived           = true
   create_default_branch = false
 }
