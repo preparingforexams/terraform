71db0bf86c0b4fc335d64d2baa7c3ee122b3189e
Björn Petersen <git@bjoernpetersen.net>
2023-10-04 13:52:10 +0200

    Drop Python 3.9 requirement on rate_limiter

--- rate_limiter.tf
+++ rate_limiter.tf
@@ -2,10 +2,8 @@ module "rate_limiter_repo" {
   source = "github.com/BlindfoldedSurgery/terraform-repo-module"
   name   = "rate_limiter"
   required_status_checks = [
-    "test (3.9)",
     "test (3.10)",
     "test (3.11)",
-    "lint (3.9)",
     "lint (3.10)",
     "lint (3.11)",
   ]
