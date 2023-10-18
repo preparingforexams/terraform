2f4606c389f6073027bf8652d5d7589cb84816d0
Björn Petersen <git@bjoernpetersen.net>
2023-10-18 16:36:42 +0200

    Make deletespamtelegrambot private

--- deletespamtelegrambot.tf
+++ deletespamtelegrambot.tf
@@ -1,9 +1,11 @@
 module "deletespam_repo" {
-  source                = "github.com/BlindfoldedSurgery/terraform-repo-module"
-  name                  = "deletespamtelegrambot"
-  default_branch_name   = "master"
-  blocked_branches      = []
-  enable_actions        = false
-  is_archived           = true
-  create_default_branch = false
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module"
+  name                   = "deletespamtelegrambot"
+  default_branch_name    = "master"
+  blocked_branches       = []
+  enable_actions         = false
+  is_archived            = true
+  create_default_branch  = false
+  protect_default_branch = false
+  is_public              = false
 }
