7ba7c94d42eed481a3fe543f233d72c751e2d2a9
Björn Petersen <git@bjoernpetersen.net>
2023-10-18 16:32:07 +0200

    Formatting

--- annoying-mention-echo-bot.tf
+++ annoying-mention-echo-bot.tf
@@ -1,10 +1,10 @@
 module "annoying_repo" {
-  source                = "github.com/BlindfoldedSurgery/terraform-repo-module"
-  name                  = "annoying-mention-echo-bot"
-  default_branch_name   = "master"
-  blocked_branches      = []
-  enable_actions        = false
-  is_archived           = true
-  create_default_branch = false
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module"
+  name                   = "annoying-mention-echo-bot"
+  default_branch_name    = "master"
+  blocked_branches       = []
+  enable_actions         = false
+  is_archived            = true
+  create_default_branch  = false
   protect_default_branch = false
 }
