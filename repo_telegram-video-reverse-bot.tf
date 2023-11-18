71bc2fc41c48cd0b984e3aa4a9dfea0fe79b42bf
Björn Petersen <git@bjoernpetersen.net>
2023-11-18 16:10:05 +0100

    Make video-reverse-bot public

--- repo_telegram-video-reverse-bot.tf
+++ repo_telegram-video-reverse-bot.tf
@@ -2,5 +2,4 @@ module "video_reverse_bot_repo" {
   source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v5.0.0"
   name        = "telegram-video-reverse-bot"
   description = "A Telegram bot that reverses videos you send it."
-  is_public   = false
 }
