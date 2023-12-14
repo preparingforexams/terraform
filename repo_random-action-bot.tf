35a6a60a4c1723c0de9f24c62a7cc9c109cb7323
Torben Carstens <git@carstens.tech>
2023-12-14 20:26:20 +0100

    Remove random_action_bot repo until it's imported

--- repo_random-action-bot.tf
+++
@@ -1,5 +0,0 @@
-module "randomactionbot_repo" {
-  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v5.0.0"
-  name                   = "random-action-bot"
-  required_status_checks = ["lint", "build-container-image"]
-}
