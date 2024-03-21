6c51d77006337ce345077e6a36df88f5d16a50c9
Björn Petersen <git@bjoernpetersen.net>
2024-03-21 12:46:13 +0100

    Fix freedom required status checks

--- repo_cure-freedom-bot.tf
+++ repo_cure-freedom-bot.tf
@@ -1,4 +1,9 @@
 module "curefreedom_repo" {
   source = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v6.0.0"
   name   = "cure-freedom-bot"
+  required_status_checks = [
+    "helm-lint",
+    "validate-renovate-config / validate",
+    "build-container-image",
+  ]
 }
