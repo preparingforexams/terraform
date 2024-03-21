4d155fa795be60335d68f62aafeb688bcd6c283e
Björn Petersen <git@bjoernpetersen.net>
2024-03-21 12:52:09 +0100

    fix(songlinker): status checks

--- repo_telegram-songlinker-bot.tf
+++ repo_telegram-songlinker-bot.tf
@@ -2,8 +2,9 @@ module "songlinker_repo" {
   source = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v6.0.0"
   name   = "telegram-songlinker-bot"
   required_status_checks = [
-    "lint",
+    "lint / lint",
+    "integration-test",
     "unit-test",
-    "build-container-image",
+    "build-container-image / build",
   ]
 }
