e819707b67c4a11cd5064592caaca002ba2c299f
Björn Petersen <git@bjoernpetersen.net>
2024-04-06 13:04:31 +0200

    Add station-bot

---
+++ repo_station-bot.tf
@@ -0,0 +1,8 @@
+module "station_bot_repo" {
+  source = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.1.0"
+  name   = "station-bot"
+  required_status_checks = [
+    "lint",
+    "build",
+  ]
+}
