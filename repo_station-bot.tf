0a0444f6f24e1746739ff1914f9d0ae8efff1fc1
Björn Petersen <git@bjoernpetersen.net>
2024-04-06 14:16:21 +0200

    Add more required status checks for station-bot

--- repo_station-bot.tf
+++ repo_station-bot.tf
@@ -1,6 +1,10 @@
 module "station_bot_repo" {
-  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.1.0"
-  name                   = "station-bot"
-  required_status_checks = []
-  enable_argocd_rules    = true
+  source = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.1.0"
+  name   = "station-bot"
+  required_status_checks = [
+    "test / test",
+    "build-container-image / build",
+    "validate-renovate-config / validate",
+  ]
+  enable_argocd_rules = true
 }
