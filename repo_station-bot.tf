5c4367d63c7970c83b500e2c0395ed19d9159fd1
Björn Petersen <git@bjoernpetersen.net>
2024-04-06 13:25:13 +0200

    Fix rules for station-bot

--- repo_station-bot.tf
+++ repo_station-bot.tf
@@ -1,8 +1,6 @@
 module "station_bot_repo" {
-  source = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.1.0"
-  name   = "station-bot"
-  required_status_checks = [
-    "lint",
-    "build",
-  ]
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.1.0"
+  name                   = "station-bot"
+  required_status_checks = []
+  enable_argocd_rules    = true
 }
