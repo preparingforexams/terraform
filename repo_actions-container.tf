db555a48c7e38d46b632fe77a74aee42a974f3fd
Björn Petersen <git@bjoernpetersen.net>
2024-05-26 19:06:52 +0200

    Fix typo

--- repo_actions-container.tf
+++ repo_actions-container.tf
@@ -1,6 +1,6 @@
 module "actions_container_repo" {
   source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.1.1"
-  name                   = "action-container"
+  name                   = "actions-container"
   description            = "Mirrors the tags of the BlindfoldedSurgery repo with the same name"
   is_public              = false
   required_status_checks = []
