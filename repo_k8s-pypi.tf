c91d9b0a6712fdbe7ef1ef421d0e91e039009c04
Björn Petersen <git@bjoernpetersen.net>
2024-06-22 20:35:56 +0200

    pypi: Enable actions

--- repo_k8s-pypi.tf
+++ repo_k8s-pypi.tf
@@ -2,7 +2,7 @@ module "pypi_repo" {
   source = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.1.1"
   name   = "k8s-pypi"
   required_status_checks = [
+    "validate-renovate-config / validate",
     "build-container-image / build",
   ]
-  enable_actions = false
 }
