c4f820cf0e85f21a2925e7ebd85a1d630a5652cc
Björn Petersen <git@bjoernpetersen.net>
2024-08-27 11:29:50 +0200

    Formatting

--- repo_k8s-pypi.tf
+++ repo_k8s-pypi.tf
@@ -1,6 +1,6 @@
 module "pypi_repo" {
-  source = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.2.0"
-  name   = "k8s-pypi"
+  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.2.0"
+  name        = "k8s-pypi"
   description = "Custom PyPi registry"
   required_status_checks = [
     "validate-renovate-config / validate",
