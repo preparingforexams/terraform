82f7abf82f6e8435b5c504fdaa433c4f7865f973
Björn Petersen <git@bjoernpetersen.net>
2024-08-27 11:44:06 +0200

    Restore papyrus repos

---
+++ repogroup_papyrus.tf
@@ -0,0 +1,19 @@
+module "papyrus_api_repo" {
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.2.0"
+  name                   = "papyrus-api"
+  default_branch_name    = "master"
+  blocked_branches       = []
+  required_status_checks = ["lint", "build_docker"]
+  is_archived            = true
+  enable_argocd_rules    = false
+}
+
+module "papyrus_bot_repo" {
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.2.0"
+  name                   = "papyrus-bot"
+  default_branch_name    = "master"
+  blocked_branches       = []
+  required_status_checks = ["lint", "build"]
+  is_archived            = true
+  enable_argocd_rules    = false
+}
