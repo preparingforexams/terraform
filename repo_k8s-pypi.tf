8c85e05037906d0891f5f2ea739161fb588f12ae
Björn Petersen <git@bjoernpetersen.net>
2024-08-27 11:29:42 +0200

    Add description

--- repo_k8s-pypi.tf
+++ repo_k8s-pypi.tf
@@ -1,6 +1,7 @@
 module "pypi_repo" {
   source = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.2.0"
   name   = "k8s-pypi"
+  description = "Custom PyPi registry"
   required_status_checks = [
     "validate-renovate-config / validate",
     "build-container-image / build",
