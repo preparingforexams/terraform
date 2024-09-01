fd07334d9f78dd57bc0e39317cc9dec1acd13fb3
Björn Petersen <git@bjoernpetersen.net>
2024-09-01 11:33:36 +0200

    rate_limiter: Reduce requirements

--- repo_rate_limiter.tf
+++ repo_rate_limiter.tf
@@ -3,17 +3,17 @@ module "rate_limiter_repo" {
   name        = "rate_limiter"
   description = "Provides scaffolding for rate limited features"
   required_status_checks = [
-    "build-migrations-container-image (amd64) / build",
-    "build-migrations-container-image (arm64) / build",
+    "build-migrations-container-image (sqlite, amd64) / build",
+    "build-migrations-container-image (sqlite, arm64) / build",
     "codecov/patch",
     "check-commits / check",
     "lint (3.11) / lint",
     "lint (3.12) / lint",
     "test (3.11) / test",
     "test (3.12) / test",
-    "test-migrations-postgres (14)",
-    "test-migrations-postgres (15)",
-    "test-migrations-postgres (16)",
+    #     "test-migrations-postgres (14)",
+    #     "test-migrations-postgres (15)",
+    #     "test-migrations-postgres (16)",
     "test-migrations-sqlite",
     "validate-renovate-config / validate",
   ]
