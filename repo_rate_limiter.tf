97eda825991b67925a9741a30997048fe70f4bdd
Björn Petersen <git@bjoernpetersen.net>
2024-09-01 11:45:35 +0200

    Require postgres checks to pass

--- repo_rate_limiter.tf
+++ repo_rate_limiter.tf
@@ -3,6 +3,8 @@ module "rate_limiter_repo" {
   name        = "rate_limiter"
   description = "Provides scaffolding for rate limited features"
   required_status_checks = [
+    "build-migrations-container-image (postgres, amd64) / build",
+    "build-migrations-container-image (postgres, arm64) / build",
     "build-migrations-container-image (sqlite, amd64) / build",
     "build-migrations-container-image (sqlite, arm64) / build",
     "codecov/patch",
@@ -11,9 +13,9 @@ module "rate_limiter_repo" {
     "lint (3.12) / lint",
     "test (3.11) / test",
     "test (3.12) / test",
-    #     "test-migrations-postgres (14)",
-    #     "test-migrations-postgres (15)",
-    #     "test-migrations-postgres (16)",
+    "test-migrations-postgres (14)",
+    "test-migrations-postgres (15)",
+    "test-migrations-postgres (16)",
     "test-migrations-sqlite",
     "validate-renovate-config / validate",
   ]
