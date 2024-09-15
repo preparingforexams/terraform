1f7e4bb42d5d956ac8881f09f467bb2496c61624
Björn Petersen <git@bjoernpetersen.net>
2024-09-15 14:08:59 +0200

    Archive probability-bot

--- repo_telegram-probability-bot.tf
+++ repo_telegram-probability-bot.tf
@@ -2,4 +2,5 @@ module "probability_repo" {
   source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.3.2"
   name        = "probability-bot"
   description = "Tracks distribution of random slot machine emoji in Telegram"
+  is_archived = true
 }
