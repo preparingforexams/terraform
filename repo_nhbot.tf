1b871cf4d70d7017ad6a114f6c07ae5b159766e1
Björn Petersen <git@bjoernpetersen.net>
2024-09-15 16:58:06 +0200

    Prepare nhbot for retirement

--- repo_nhbot.tf
+++ repo_nhbot.tf
@@ -2,5 +2,9 @@ module "nh_repo" {
   source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.3.2"
   name                   = "nhbot"
   description            = "nh"
-  required_status_checks = ["lint", "build"]
+  required_status_checks = []
+  protect_default_branch = false
+  blocked_branches       = []
+  enable_actions         = false
+  enable_argocd_rules    = false
 }
