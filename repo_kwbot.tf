3372242c7e8f89a1887c8df9cb2e643418cb620e
Björn Petersen <git@bjoernpetersen.net>
2024-09-15 16:53:22 +0200

    Prepare kwbot for retirement

--- repo_kwbot.tf
+++ repo_kwbot.tf
@@ -1,5 +1,9 @@
 module "kwbot_repo" {
   source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.3.2"
   name                   = "kwbot"
-  required_status_checks = ["lint", "build"]
+  required_status_checks = []
+  protect_default_branch = false
+  blocked_branches       = []
+  enable_actions         = false
+  enable_argocd_rules    = false
 }
