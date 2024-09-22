176eab368b6572cbe002740dfa079137f0172b56
Björn Petersen <git@bjoernpetersen.net>
2024-09-22 14:20:15 +0200

    Prepare freedom bot for retirement

--- repo_cure-freedom-bot.tf
+++ repo_cure-freedom-bot.tf
@@ -1,10 +1,9 @@
 module "curefreedom_repo" {
   source              = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.3.2"
   name                = "cure-freedom-bot"
-  enable_argocd_rules = true
-  required_status_checks = [
-    "helm-lint",
-    "validate-renovate-config / validate",
-    "build-container-image / build",
-  ]
+  enable_argocd_rules = false
+  protect_default_branch = false
+  enable_actions         = false
+  blocked_branches = []
+  required_status_checks = [  ]
 }
