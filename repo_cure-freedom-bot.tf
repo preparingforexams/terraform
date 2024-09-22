bd609e5f043eda02f01209f0a56ceee29313adb4
Björn Petersen <git@bjoernpetersen.net>
2024-09-22 14:21:06 +0200

    Move freedom bot to retirement group

--- repo_cure-freedom-bot.tf
+++
@@ -1,9 +0,0 @@
-module "curefreedom_repo" {
-  source              = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.3.2"
-  name                = "cure-freedom-bot"
-  enable_argocd_rules = false
-  protect_default_branch = false
-  enable_actions         = false
-  blocked_branches = []
-  required_status_checks = [  ]
-}
--- repogroup_retired.tf
+++ repogroup_retired.tf
@@ -34,3 +34,15 @@ module "kwbot_repo" {
   is_archived            = true
   is_public              = false
 }
+
+module "curefreedom_repo" {
+  source = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.3.2"
+  name   = "cure-freedom-bot"
+
+  enable_argocd_rules    = false
+  protect_default_branch = false
+  enable_actions         = false
+
+  blocked_branches       = []
+  required_status_checks = []
+}
