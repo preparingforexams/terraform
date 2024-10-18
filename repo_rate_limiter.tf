47d6c0188efbb9db1bc617ec39f1f3e11bc375db
Björn Petersen <git@bjoernpetersen.net>
2024-10-18 12:08:28 +0200

    rate-limiter: Require Python 3.13 checks

--- repo_rate_limiter.tf
+++ repo_rate_limiter.tf
@@ -2,6 +2,7 @@ module "rate_limiter_repo" {
   source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.3.3"
   name        = "rate_limiter"
   description = "Provides scaffolding for rate limited features"
+
   required_status_checks = [
     "build-migrations-container-image (postgres, amd64) / build",
     "build-migrations-container-image (postgres, arm64) / build",
@@ -11,8 +12,10 @@ module "rate_limiter_repo" {
     "check-commits / check",
     "lint (3.11) / lint",
     "lint (3.12) / lint",
+    "lint (3.13) / lint",
     "test (3.11) / test",
     "test (3.12) / test",
+    "test (3.13) / test",
     "test-migrations-postgres (14)",
     "test-migrations-postgres (15)",
     "test-migrations-postgres (16)",
