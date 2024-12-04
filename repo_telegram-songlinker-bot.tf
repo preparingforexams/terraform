6fe99b0046d11ec665396fdb190f98ac1ab80c27
Björn Petersen <git@bjoernpetersen.net>
2024-12-04 22:26:26 +0100

    songlinker: Update status checks

--- repo_telegram-songlinker-bot.tf
+++ repo_telegram-songlinker-bot.tf
@@ -1,12 +1,13 @@
 module "songlinker_repo" {
-  source              = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.3.3"
-  name                = "telegram-songlinker-bot"
-  description         = "Provides links to songs on several music streaming services."
-  enable_argocd_rules = true
+  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.3.3"
+  name        = "telegram-songlinker-bot"
+  description = "Provides links to songs on several music streaming services."
+
   required_status_checks = [
+    "validate-renovate-config / validate",
     "lint / lint",
-    "integration-test",
-    "unit-test",
+    "integration-test / test",
+    "unit-test / test",
     "build-container-image / build",
   ]
 }
