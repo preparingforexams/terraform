aa57530b811a2b223922b4fe1e073dc1c16ce55a
Björn Petersen <git@bjoernpetersen.net>
2024-12-15 01:27:55 +0100

    Add repo for mood bot

---
+++ repo_telegram-mood-bot.tf
@@ -0,0 +1,12 @@
+module "mood_repo" {
+  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v8.0.2"
+  name        = "telegram-mood-bot"
+  description = "Tracks people's mood."
+
+  required_status_checks = [
+    "lint / lint",
+    "test / test",
+    "build-container-image (., app) / build",
+    "build-container-image (migrations, migrations) / build",
+  ]
+}
