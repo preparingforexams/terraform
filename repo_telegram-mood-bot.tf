780c0da1ae9bf024c192a4bf3006a10a8853c403
Björn Petersen <git@bjoernpetersen.net>
2025-01-07 18:08:40 +0100

    Use standard checks for mood-bot

--- repo_telegram-mood-bot.tf
+++ repo_telegram-mood-bot.tf
@@ -2,11 +2,4 @@ module "mood_repo" {
   source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v9.0.0"
   name        = "telegram-mood-bot"
   description = "Tracks people's mood."
-
-  required_status_checks = [
-    "lint / lint",
-    "test / test",
-    "build-container-image (., app) / build",
-    "build-container-image (migrations, migrations) / build",
-  ]
 }
