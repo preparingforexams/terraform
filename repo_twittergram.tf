340140e9517fb2c166d93f38d1ef59412e0d1bc2
Björn Petersen <git@bjoernpetersen.net>
2025-05-20 10:17:52 +0200

    twittergram: remove sonarqube

--- repo_twittergram.tf
+++ repo_twittergram.tf
@@ -2,10 +2,4 @@ module "twittergram_repo" {
   source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v9.3.0"
   name        = "twittergram"
   description = "Forwards toots/mails to Telegram chats"
-  required_status_checks = [
-    "lint / lint",
-    "test / test",
-    "post-build-container-image",
-    "SonarCloud Code Analysis",
-  ]
 }
