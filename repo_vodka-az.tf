8eabd3d942c26245b75c035907e89b02e0e9d4be
Björn Petersen <git@bjoernpetersen.net>
2025-08-08 16:54:32 +0200

    Prepare vodka AZ for archival

--- repo_vodka-az.tf
+++ repo_vodka-az.tf
@@ -2,4 +2,10 @@ module "vodka_repo" {
   source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v9.3.0"
   name        = "vodka-az"
   description = "Suggests long drink combinations with Vodka"
+
+  is_archived                  = false
+  include_required_meta_checks = false
+  enable_argocd_rules          = false
+  protect_default_branch       = false
+  required_status_checks       = []
 }
