cfdf2c14fea8875e95445a9a2d4750d20cc980f7
Björn Petersen <git@bjoernpetersen.net>
2025-10-23 21:14:51 +0200

    chore(rate_limter): forbid ruleset bypass

--- repo_rate_limiter.tf
+++ repo_rate_limiter.tf
@@ -3,6 +3,8 @@ module "rate_limiter_repo" {
   name        = "rate_limiter"
   description = "Provides scaffolding for rate limited features"
 
+  allow_default_branch_protection_bypass = false
+
   required_status_checks = [
     "post-build-migrations-container-image",
     "check-commits / check",
