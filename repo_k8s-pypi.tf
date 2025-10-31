9bb69521241005e6f7029156d227e0f73ddd59b8
Björn Petersen <git@bjoernpetersen.net>
2025-10-31 13:14:56 +0100

    chore: remove k8s-pypi

--- repo_k8s-pypi.tf
+++
@@ -1,8 +0,0 @@
-module "pypi_repo" {
-  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v10.1.1"
-  name        = "k8s-pypi"
-  description = "Custom PyPi registry"
-  required_status_checks = [
-    "post-build-container-image",
-  ]
-}
