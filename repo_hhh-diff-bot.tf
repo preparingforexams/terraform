10d46495798bb5cc39656e63db420e07d595780a
Björn Petersen <git@bjoernpetersen.net>
2025-11-02 22:26:35 +0100

    Archive hhhdiffbot

--- repo_hhh-diff-bot.tf
+++ repo_hhh-diff-bot.tf
@@ -3,6 +3,7 @@ module "hhh_diff_bot_repo" {
   name        = "hhh-diff-bot"
   description = "Keeps a list of all Telegram group chats it's invited to"
 
+  is_archived         = true
   is_archive_prepared = true
   is_public           = false
 }
