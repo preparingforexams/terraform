e5033797f94c5654d87ca799b8bfaa53c3ccf9e2
Björn Petersen <git@bjoernpetersen.net>
2025-11-02 22:22:15 +0100

    Prepare hhhdiffbot for archival

--- repo_hhh-diff-bot.tf
+++ repo_hhh-diff-bot.tf
@@ -2,4 +2,6 @@ module "hhh_diff_bot_repo" {
   source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v10.1.1"
   name        = "hhh-diff-bot"
   description = "Keeps a list of all Telegram group chats it's invited to"
+
+  is_archive_prepared = true
 }
