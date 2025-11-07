d964913d246d50a3063a0c491b9e782c6f47c867
Björn Petersen <git@bjoernpetersen.net>
2025-11-07 22:44:43 +0100

    Archive welcome-bot

--- repo_telegram-welcome-bot.tf
+++ repo_telegram-welcome-bot.tf
@@ -3,6 +3,7 @@ module "welcome_repo" {
   name        = "telegram-welcome-bot"
   description = "Welcomes users to a group chat"
 
+  is_archived         = true
   is_archive_prepared = true
   is_public           = false
 }
