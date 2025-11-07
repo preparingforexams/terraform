065a95ca4dea634c39c5c8f0657ee069545ba1aa
Björn Petersen <git@bjoernpetersen.net>
2025-11-07 22:31:16 +0100

    welcome: prepare for archival

--- repo_telegram-welcome-bot.tf
+++ repo_telegram-welcome-bot.tf
@@ -2,5 +2,6 @@ module "welcome_repo" {
   source              = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v10.1.1"
   name                = "telegram-welcome-bot"
   description         = "Welcomes users to a group chat"
-  enable_argocd_rules = true
+
+  is_archive_prepared = true
 }
