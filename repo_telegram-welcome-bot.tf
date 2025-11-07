210dcc5c4b95d58b3320daad388c95a781f864a1
Björn Petersen <git@bjoernpetersen.net>
2025-11-07 22:32:41 +0100

    style: formatting

--- repo_telegram-welcome-bot.tf
+++ repo_telegram-welcome-bot.tf
@@ -1,7 +1,7 @@
 module "welcome_repo" {
-  source              = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v10.1.1"
-  name                = "telegram-welcome-bot"
-  description         = "Welcomes users to a group chat"
+  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v10.1.1"
+  name        = "telegram-welcome-bot"
+  description = "Welcomes users to a group chat"
 
   is_archive_prepared = true
 }
