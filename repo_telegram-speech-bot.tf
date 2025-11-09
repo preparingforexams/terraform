0ef88e70fb75285e1a70ce1d7090b0c9d215ac84
Björn Petersen <git@bjoernpetersen.net>
2025-11-09 11:00:58 +0100

    Archive speech bot

--- repo_telegram-speech-bot.tf
+++ repo_telegram-speech-bot.tf
@@ -4,26 +4,8 @@ module "bob_repo" {
   source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v10.1.1"
   name        = "telegram-speech-bot"
   description = "TTS for all your annoyance needs"
-}
 
-resource "google_service_account" "bob_bot" {
-  account_id   = "bob-bot"
-  display_name = "Bob Bot"
-}
-
-resource "google_project_iam_member" "bob_policies" {
-  for_each = toset([
-    "roles/cloudtranslate.user",
-    "roles/datastore.user",
-  ])
-
-  project = google_service_account.bob_bot.project
-  role    = each.key
-  member  = google_service_account.bob_bot.member
-}
-
-module "bob_gsa_secret" {
-  source                    = "./modules/gsa_secret"
-  google_service_account_id = google_service_account.bob_bot.account_id
-  scaleway_project_id       = "51c6a6f0-ffaa-49b2-8285-745772763e07"
+  is_archive_prepared = true
+  is_archived         = true
+  is_public           = false
 }
